3d1698998c039c866b35a123dfb974a92445b1c2
Arjeeah <argea2004@gmail.com>
2024-02-25 18:04:07 +0200

    marvel 1

---
+++ lib/helpers/consts.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/widgets.dart';
+
+String basUrl = "";
+Color primaryColor = const Color(0xffF0141E);
---
+++ lib/screens/detailed_movie.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DetailedScreens extends StatefulWidget {
+  const DetailedScreens({super.key});
+
+  @override
+  State<DetailedScreens> createState() => _DetailedScreensState();
+}
+
+class _DetailedScreensState extends State<DetailedScreens> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
